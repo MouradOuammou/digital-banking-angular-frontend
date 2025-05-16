8b5b0e5924d426ec33a213a8f5cfdc06ac7a57dc
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-16 15:03:54 +0100

    completer component customer-account

--- src/app/customer-accounts/customer-accounts.component.ts
+++ src/app/customer-accounts/customer-accounts.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {Customer} from "../model/customer.model";
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-customer-accounts',
-  imports: [],
   templateUrl: './customer-accounts.component.html',
-  styleUrl: './customer-accounts.component.css'
+  imports: [
+    JsonPipe
+  ],
+  styleUrls: ['./customer-accounts.component.css']
 })
-export class CustomerAccountsComponent {
+export class CustomerAccountsComponent implements OnInit {
+  customerId! : string ;
+  customer! : Customer;
+  constructor(private route : ActivatedRoute, private router :Router) {
+    this.customer=this.router.getCurrentNavigation()?.extras.state as Customer;
+  }
+
+  ngOnInit(): void {
+    this.customerId = this.route.snapshot.params['id'];
+
+  }
 
 }
