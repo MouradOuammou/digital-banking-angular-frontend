5b899dfca997bd8a6d13d39f6d38fca39f789e50
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-25 15:37:51 +0100

    configurer login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    NgClass,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  formLogin!: FormGroup;
+  loginForm: any;
+  isLoading: false | undefined;
 
+  constructor(private fb: FormBuilder, private router: Router) { }
+    ngOnInit(): void {
+      this.formLogin = this.fb.group({
+        username: this.fb.control(''),
+        password: this.fb.control('')
+      })
+    }
+
+  onSubmit() {
+
+  }
 }
