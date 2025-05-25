402efb45e12022380fce35edba6689851ef0e4a1
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-25 15:39:07 +0100

    configurer root

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { CustomersComponent } from "./customers/customers.component";
 import { AccountsComponent } from "./accounts/accounts.component";
 import { NewCustomerComponent } from "./new-customer/new-customer.component";
 import { CustomerAccountsComponent } from "./customer-accounts/customer-accounts.component";
+import {LoginComponent} from './login/login.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'customers', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'customers', component: CustomersComponent },
   { path: 'accounts', component: AccountsComponent },
   { path: 'new-customer', component: NewCustomerComponent },
